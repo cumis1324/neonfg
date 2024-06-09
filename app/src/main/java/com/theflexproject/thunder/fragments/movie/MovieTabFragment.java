34aa85b2079a5749fa19be8e6c0d187b1f8b9051
cumis ganteng <admin@nfgplus.my.id>
2024-06-09 21:56:59 +0700

    movie for you

---
+++ app/src/main/java/com/theflexproject/thunder/fragments/movie/MovieTabFragment.java
@@ -0,0 +1,23 @@
+package com.theflexproject.thunder.fragments.movie;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.theflexproject.thunder.R;
+
+class MovieTabFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             @Nullable ViewGroup container,
+                             @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_movie_tab, container, false);
+    }
+}
