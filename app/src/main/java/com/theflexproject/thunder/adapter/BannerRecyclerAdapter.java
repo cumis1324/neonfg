a7c0a4a928b53bc0f71a883803d006c5075b567d
cumis ganteng <admin@nfgplus.my.id>
2024-06-11 04:29:08 +0700

    ui resume banner adapter

--- app/src/main/java/com/theflexproject/thunder/adapter/BannerRecyclerAdapter.java
+++ app/src/main/java/com/theflexproject/thunder/adapter/BannerRecyclerAdapter.java
@@ -2,12 +2,14 @@ package com.theflexproject.thunder.adapter;
 
 import static com.theflexproject.thunder.Constants.TMDB_IMAGE_BASE_URL;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -19,7 +21,13 @@ import com.bumptech.glide.load.engine.DiskCacheStrategy;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.bumptech.glide.request.target.Target;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.theflexproject.thunder.R;
+import com.theflexproject.thunder.model.FirebaseManager;
 import com.theflexproject.thunder.model.Movie;
 
 import java.util.List;
@@ -28,11 +36,23 @@ public class BannerRecyclerAdapter extends RecyclerView.Adapter<BannerRecyclerAd
 
     Context context;
     List<Movie> mediaList;
-    private BannerRecyclerAdapter.OnItemClickListener listener;
-    public BannerRecyclerAdapter(Context context, List<Movie> mediaList, BannerRecyclerAdapter.OnItemClickListener listener) {
+    private OnItemClickListener listener;
+    FirebaseManager manager;
+    private DatabaseReference databaseReference;
+
+    public BannerRecyclerAdapter(Context context, List<Movie> mediaList, OnItemClickListener listener) {
         this.context = context;
         this.mediaList = mediaList;
         this.listener = listener;
+        this.manager = new FirebaseManager();
+        databaseReference = FirebaseDatabase.getInstance().getReference("History");
+        setHasStableIds(true); // Enable stable IDs
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        // Return a unique view type for each item position
+        return position;
     }
 
     @NonNull
@@ -43,64 +63,103 @@ public class BannerRecyclerAdapter extends RecyclerView.Adapter<BannerRecyclerAd
     }
 
     @Override
-    public void onBindViewHolder(@NonNull MovieViewHolder holder, int position) {
-        if(mediaList.get(position).getBackdrop_path()!=null){
-            holder.name.setText(mediaList.get(position).getTitle());
+    public void onBindViewHolder(@NonNull MovieViewHolder holder, @SuppressLint("RecyclerView") int position) {
+        Movie movie = mediaList.get(position);
+
+        holder.name.setText(movie.getTitle());
+
+        if (movie.getBackdrop_path() != null) {
             Glide.with(context)
-                    .load(TMDB_IMAGE_BASE_URL +mediaList.get(position).getBackdrop_path())
+                    .load(TMDB_IMAGE_BASE_URL + movie.getBackdrop_path())
                     .placeholder(new ColorDrawable(Color.BLACK))
                     .apply(RequestOptions.bitmapTransform(new RoundedCorners(14)))
                     .into(holder.poster);
 
-            if(mediaList.get(position).getLogo_path()!=null){
+            if (movie.getLogo_path() != null) {
                 holder.logo.setVisibility(View.VISIBLE);
                 holder.name.setVisibility(View.GONE);
                 Glide.with(context)
-                        .load(TMDB_IMAGE_BASE_URL +mediaList.get(position).getLogo_path())
+                        .load(TMDB_IMAGE_BASE_URL + movie.getLogo_path())
                         .apply(new RequestOptions()
                                 .fitCenter()
                                 .override(Target.SIZE_ORIGINAL))
                         .diskCacheStrategy(DiskCacheStrategy.ALL)
                         .placeholder(new ColorDrawable(Color.TRANSPARENT))
                         .into(holder.logo);
-            }
-            if(mediaList.get(position).getLogo_path()==null){
-                holder.name.setText(mediaList.get(position).getTitle());
+            } else {
                 holder.name.setVisibility(View.VISIBLE);
             }
         }
 
+        String tmdbId = String.valueOf(mediaList.get(position).getId());
+        String userId = manager.getCurrentUser().getUid();
+        databaseReference = FirebaseDatabase.getInstance().getReference("History");
+        DatabaseReference userReference = databaseReference.child(tmdbId + "/" + userId);
+        DatabaseReference p = userReference.child("lastPosition");
+
+        p.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.exists()) {
+                    Long lastPosition = dataSnapshot.getValue(Long.class);
+                    if (lastPosition != null) {
+                        long runtime = (long) mediaList.get(position).getRuntime() * 60 * 1000;
+                        double progress = (double) lastPosition / runtime;
+
+                        holder.poster.getViewTreeObserver().addOnGlobalLayoutListener(new ViewTreeObserver.OnGlobalLayoutListener() {
+                            @Override
+                            public void onGlobalLayout() {
+                                holder.poster.getViewTreeObserver().removeOnGlobalLayoutListener(this);
+
+                                int progressWidth = (int) (holder.poster.getWidth() * progress);
+                                holder.progressOverlay.getLayoutParams().width = progressWidth;
+                                holder.progressOverlay.requestLayout();
+                            }
+                        });
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Handle onCancelled event
+            }
+        });
     }
 
     @Override
     public int getItemCount() {
-        return (mediaList==null)?0:mediaList.size();
+        return (mediaList == null) ? 0 : mediaList.size();
     }
 
-    public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    @Override
+    public long getItemId(int position) {
+        return position; // Return a unique ID for each item
+    }
+
+    public class MovieViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         TextView name;
         ImageView poster;
         ImageView logo;
+        View progressOverlay;
 
         public MovieViewHolder(@NonNull View itemView) {
             super(itemView);
+            progressOverlay = itemView.findViewById(R.id.progress_overlay2);
             logo = itemView.findViewById(R.id.movieLogo);
             name = itemView.findViewById(R.id.textView4);
-            poster= itemView.findViewById(R.id.moviePoster);
+            poster = itemView.findViewById(R.id.moviePoster);
             itemView.setOnClickListener(this);
-
         }
-
 
         @Override
         public void onClick(View v) {
-            listener.onClick(v,getAbsoluteAdapterPosition());
+            listener.onClick(v, getAbsoluteAdapterPosition());
         }
     }
 
     public interface OnItemClickListener {
-        public void onClick(View view, int position);
+        void onClick(View view, int position);
     }
-
 }
